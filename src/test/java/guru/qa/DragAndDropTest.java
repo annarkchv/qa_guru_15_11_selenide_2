5c4306bf5b7525813762b4f39c62d4684a16c436
AnnaRkchv <rakchaevaa@gmail.com>
2022-11-01 00:12:21 +0200

    check initial positions of the rectangles

--- src/test/java/guru/qa/DragAndDropTest.java
+++ src/test/java/guru/qa/DragAndDropTest.java
@@ -1,17 +1,19 @@
 package guru.qa;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DragAndDropTest {
 
     @Test
     void replaceRectangles() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
         $("#column-a").dragAndDropTo("#column-b");
-        $("#column-a").shouldHave(Condition.text("B"));
+        $("#column-a").shouldHave(text("B"));
 
     }
 }
